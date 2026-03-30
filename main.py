0dc15e08e6746a5c5f0f77f140e691f669455801
Timofey Lialuskin <timalaluskin@gmail.com>
2026-03-30 19:55:44 +0300

    -Created EmailSender class

--- main.py
+++ main.py
@@ -1,40 +1,45 @@
-from fastapi import FastAPI
+import aiosqlite
 from dotenv import load_dotenv
-import asyncio
-import aiosqlite as sq
 import os
-
-load_dotenv('.env')
-DATABASE_NAME = os.getenv("DATABASE_NAME")
-api = FastAPI()
+import asyncio
 
 
-async def init_db():
-    async with sq.connect(DATABASE_NAME) as db:
-        await db.execute('''CREATE TABLE IF NOT EXISTS users (
+
+class EmailSender:
+    def __init__(self, db_name: str = None):
+        load_dotenv('.env')
+        if db_name is None:
+            self.DB_NAME = os.getenv('EMAIL_DATABASE_NAME')
+        else:
+            self.DB_NAME = db_name
+
+    async def init_db(self) -> None:
+        async with aiosqlite.connect(self.DB_NAME) as db:
+            await db.execute('''
+        CREATE TABLE IF NOT EXISTS users (
         id INTEGER PRIMARY KEY AUTOINCREMENT,
-        chat_id INTEGER UNIQUE,
-        username TEXT
-        )
-        ''')
-        await db.commit()
+        email TEXT,
+        "group" TEXT
+        )''')
+            await db.commit()
 
+    async def add_user(self, email: str, group: str) -> None:
+        async with aiosqlite.connect(self.DB_NAME) as db:
+            await db.execute('''INSERT OR IGNORE INTO users (email, "group") VALUES (?, ?)''', (email, group))
+            await db.commit()
 
-async def add_user(chat_id: int, username: str) -> None:
-    async with sq.connect(DATABASE_NAME) as db:
-        await db.execute(
-            "INSERT OR IGNORE INTO users (chat_id, username) VALUES (?, ?)",
-            (chat_id, username)
-        )
-        await db.commit()
-        print(f"Юзер {username} обработан.")
-
+    async def get_emails(self, group: str) -> list:
+        async with aiosqlite.connect(self.DB_NAME) as db:
+            async with db.execute('''SELECT email FROM users WHERE "group" = ?''', (group,)) as cursor:
+                rows = await cursor.fetchall()
+                emails = [row[0] for row in rows]
+                return emails
 
 async def main():
-    await init_db()
-    i, name = input().split()
-    await add_user(int(i), name)
-
+    sender = EmailSender()
+    await sender.init_db()
+    await sender.add_user('timalaluskin@gmail.com', 'staff')
+    print(await sender.get_emails('staff'))
 
 if __name__ == '__main__':
     asyncio.run(main())
