21ff3cbc9b6adee0aae32044af441443f9f63931
Timofey Lialuskin <timalaluskin@gmail.com>
2026-03-30 20:20:52 +0300

    -Edited EmailSender class

--- main.py
+++ main.py
@@ -2,7 +2,8 @@ import aiosqlite
 from dotenv import load_dotenv
 import os
 import asyncio
-
+from email.message import EmailMessage
+import aiosmtplib
 
 
 class EmailSender:
@@ -17,8 +18,8 @@ class EmailSender:
         async with aiosqlite.connect(self.DB_NAME) as db:
             await db.execute('''
         CREATE TABLE IF NOT EXISTS users (
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
-        email TEXT,
+        id INTEGER UNIQUE PRIMARY KEY AUTOINCREMENT,
+        email TEXT UNIQUE,
         "group" TEXT
         )''')
             await db.commit()
@@ -35,11 +36,34 @@ class EmailSender:
                 emails = [row[0] for row in rows]
                 return emails
 
+    # async def send_mail(self, to: str) -> None:
+    #     # Формируем структуру письма
+    #     message = EmailMessage()
+    #     message["From"] = "opensender@send.org"
+    #     message["To"] = "timalaluskin@gmail.com"
+    #     message["Subject"] = "Привет из асинхронности!"
+    #     message.set_content("Это текст письма, отправленного через aiosmtplib.")
+    #
+    #     # Отправка
+    #     await aiosmtplib.send(
+    #         message,
+    #         hostname=os.getenv('MAIL_HOST'),  # Замените на ваш SMTP-сервер
+    #         port=int(os.getenv('MAIL_PORT')),
+    #         username=os.getenv("MAIL_LOGIN"),
+    #         password=os.getenv("MAIL_PASS"),
+    #         use_tls=bool(os.getenv("TLS"))
+    #     )
+
+
 async def main():
     sender = EmailSender()
     await sender.init_db()
     await sender.add_user('timalaluskin@gmail.com', 'staff')
+    await sender.add_user('ivanoduvan21@gmail.com', 'family')
     print(await sender.get_emails('staff'))
+    print(await sender.get_emails('family'))
+    # await sender.send_mail('LTE')
+
 
 if __name__ == '__main__':
     asyncio.run(main())
